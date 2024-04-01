5bf590519577fab6a951dcdea5ed19b4adee4094
chlgodnr <wieltm6540@naver.com>
2024-04-01 23:56:59 +0900

    자동차 이동상태 출력

--- src/main/java/racingcar/Running.java
+++ src/main/java/racingcar/Running.java
@@ -48,3 +48,42 @@ public class Running {
         }
         System.out.println();
     } //자동차 이동상태
+
+    public int findMaxDistance(int[] carPosition) {
+        int maxDistance = 0;
+        for (int position : carPosition) {
+            if (position > maxDistance) {
+                maxDistance = position;
+            }
+        }
+        return maxDistance;
+    } //최대거리 구하기
+
+    public int countWinner(int[] carPosition, int maxDistance) {
+        int count = 0;
+        for (int i = 0; i < carPosition.length; i++) {
+            if (maxDistance == carPosition[i]) count++;
+        }
+        return count;
+    } //우승자 수
+
+    public String[] findWinners(String[] carName, int[] carPosition, int maxDistance) {
+        int index = 0;
+        int count = countWinner(carPosition, maxDistance);
+        String[] winner = new String[count];
+        for (int i = 0; i < carName.length; i++) {
+            if (maxDistance == carPosition[i]) winner[index++] = carName[i];
+        }
+        return winner;
+    } // 우승자 찾기
+
+    public void printWinner(String[] winner) {
+        System.out.print("최종 우승자 : ");
+        if (winner.length == 1) System.out.println(winner[0]);
+        else {
+            for (int i = 0; i < winner.length - 1; i++)
+                System.out.print(winner[i] + ",");
+        }
+        System.out.println(winner[winner.length - 1]);
+    } //우승자 출력
+}
